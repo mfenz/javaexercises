bcdbf3ba5d37d836c16877de82d611fe7ee71787
Marc <mf@thefuture.at>
2021-03-01 15:31:54 +0100

    Operatoren

--- 20210301_Schneckenrennen/src/Schneckenrennen.java
+++ 20210301_Schneckenrennen/src/Schneckenrennen.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Schneckenrennen {
     private float distanz;
 
     private List<Schnecke> teilnehmer = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
 
     public Schneckenrennen(float distanz) {
         this.distanz = distanz;
@@ -17,9 +21,36 @@ public class Schneckenrennen {
     }
 
     public void addSchnecken(){
-        teilnehmer.add(new Gehaeuseschnecke("Tiger", 2.4f, "gelb"));
-        teilnehmer.add(new Gehaeuseschnecke("Speedy", 3.0f, "rot"));
-        teilnehmer.add(new Nacktschnecke("Cookie", 2.2f));
+//        teilnehmer.add(new Gehaeuseschnecke("Tiger", 2.4f, "gelb"));
+//        teilnehmer.add(new Gehaeuseschnecke("Speedy", 3.0f, "rot"));
+//        teilnehmer.add(new Nacktschnecke("Cookie", 2.2f));
+
+        System.out.println("Wie viele Nacktschnecken sollen teilnehmen?");
+        int anzahlNacktschnecken = scanner.nextInt();
+        addNacktschnecken(anzahlNacktschnecken);
+    }
+
+    private void addNacktschnecken(int anzahl){
+        for(int i = 0; i < anzahl; ++i){
+            System.out.println("Bitte den Namen der Nacktschnecke eingeben:");
+            String name = scanner.next();
+            System.out.println("Bitte speed eingeben: ");
+            float speed = readSpeed();
+            teilnehmer.add(new Nacktschnecke(name, speed));
+        }
+    }
+
+    private float readSpeed(){
+        boolean success = false;
+        while(!success){
+            try {
+                return scanner.nextFloat();
+            } catch (InputMismatchException e){
+                System.out.println("Falsche Eingabe! Bitte wiederholen:");
+                scanner.nextLine();
+            }
+        }
+        return 0.0f;
     }
 
     public void rennen(){
