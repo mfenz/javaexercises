86a28d1c608a3c40cd9775d787e605a2611fad47
Marc <mf@thefuture.at>
2021-03-23 09:57:11 +0100

    zahlen

--- 20210323_Methoden/src/Zahlen.java
+++ 20210323_Methoden/src/Zahlen.java
@@ -1,16 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Zahlen {
     // Instanzvariable
     // gehören immer zu einem Objekt einer Klasse
-    private int[] zahlen = new int[3];
+    private int[] zahlen = new int[478656878];
 
     public static void main(String[] args) {
         Zahlen z = new Zahlen();
         z.fuellen();
         z.print(z.getZahlen());
+        int[] even = z.getEven(z.getZahlen());
+        z.print(even);
     }
-    
+    private int[] getEven(int[] zahlen){
+        List<Integer> even = new ArrayList<>();
+        for(int i = 0; i < zahlen.length; i++){
+            if(zahlen[i] % 2 == 0){
+                even.add(zahlen[i]);
+            }
+        }
+        // even-Liste in ein Array umwandeln
+        int[] result = new int[even.size()];
+        for(int i = 0; i < even.size(); i++){
+            result[i] = even.get(i);
+        }
+        // result-Array zurückgeben
+        return result;
+    }
+    private void fuellen(){
+        Random random = new Random();
+        for(int i = 0; i < zahlen.length; i++){
+            // 1) Zufallswert erzeugen
+            int num = random.nextInt();
+            // Array an der Stelle i mit Zufallszahl initialisieren
+            zahlen[i] = num;
+        }
+    }
+    private void print(int[] zahlen){
+        for(int j : zahlen){
+            System.out.print(j + ", ");
+        }
+//        for(int i = 0; i < zahlen.length; i++){
+//            System.out.print(zahlen[i] + ", ");
+//        }
+        System.out.println();
+    }
+
     // Konstruktor
     public Zahlen(){
     }
@@ -23,25 +60,5 @@ public class Zahlen {
      */
     public int[] getZahlen(){
         return zahlen;
-    }
-
-    private void fuellen(){
-        Random random = new Random();
-        for(int i = 0; i < zahlen.length; i++){
-            // 1) Zufallswert erzeugen
-            int num = random.nextInt();
-            // Array an der Stelle i mit Zufallszahl initialisieren
-            zahlen[i] = num;
-        }
-    }
-
-    private void print(int[] zahlen){
-        for(int j : zahlen){
-            System.out.print(j + ", ");
-        }
-//        for(int i = 0; i < zahlen.length; i++){
-//            System.out.print(zahlen[i] + ", ");
-//        }
-        System.out.println();
     }
 }
