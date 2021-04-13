6c44271f995d0aa59ddc12d318483b2bf163316b
Marc <mf@thefuture.at>
2021-04-13 10:12:33 +0200

    ttt

--- 20210413_TTT/src/at/cb/ttt/bl/TicTacToeBl.java
+++ 20210413_TTT/src/at/cb/ttt/bl/TicTacToeBl.java
@@ -1,6 +1,5 @@
 package at.cb.ttt.bl;
 
-import at.cb.ttt.db.DatabaseManager;
 import at.cb.ttt.model.Player;
 
 import java.util.Scanner;
@@ -13,11 +12,21 @@ public class TicTacToeBl {
     // 3 Zeilen, 3 Spalten
     private Player[][] board = new Player[3][3];
 
-    // Aktiver Spieler
-    private Player currentPlayer;
+    // Aktiver Spieler, 0 oder 1
+    // Instanzvariable default 0
+    private int currentPlayer;
+
+    // Array mit Spielern
+    private Player[] players = new Player[2];
 
     // User hinzufügen
     private void addPlayers(){
+        // Spieler 1 erstellen
+        System.out.println("Spieler 1:");
+        players[0] = createPlayer();
+        // Spieler 2 erstellen
+        System.out.println("Spieler 2:");
+        players[1] = createPlayer();
     }
 
     // erstellt einen Player
@@ -45,7 +54,27 @@ public class TicTacToeBl {
 
     // Spiel starten
     public void play(){
+        // Am Anfang Spieler hinzufügen
         addPlayers();
+        board[1][1] = players[0];
+        board[0][2] = players[1];
+        printBoard();
+    }
+
+    public void printBoard(){
+        for(int zeile = 0; zeile < board.length; zeile++){
+            for(int spalte = 0; spalte < board[zeile].length; spalte++){
+                if(board[zeile][spalte] == null){
+                    // wenn noch nichts ausgewählt wurde
+                    System.out.print("-");
+                } else {
+                    // Player Symbol ausgeben
+                    System.out.print(board[zeile][spalte].getSymbol());
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
 }
