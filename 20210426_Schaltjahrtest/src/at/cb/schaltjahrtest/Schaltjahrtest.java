ada79e45d5a04cf8a6e6c917f932393d4faa3559
Marc <mf@thefuture.at>
2021-04-26 09:58:32 +0200

    Schaltjahr

---
+++ 20210426_Schaltjahrtest/src/at/cb/schaltjahrtest/Schaltjahrtest.java
@@ -0,0 +1,16 @@
+package at.cb.schaltjahrtest;
+
+public class Schaltjahrtest {
+    public static void main(String[] args) {
+        int jahr = 2021;
+        if(jahr % 4 == 0 && jahr % 100 != 0){
+            System.out.println(jahr + " ist ein Schaltjahr");
+        } else if(jahr % 100 == 0 && jahr % 400 != 0){
+            System.out.println(jahr + " ist kein Schaltjahr");
+        } else if(jahr % 400 == 0){
+            System.out.println(jahr + " ist ein Schaltjahr");
+        } else {
+            System.out.println(jahr + " ist kein Schaltjahr");
+        }
+    }
+}
