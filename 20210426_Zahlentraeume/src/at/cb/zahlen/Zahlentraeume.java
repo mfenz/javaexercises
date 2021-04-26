9b46115d0cc23bc0ba3a76c6c09a3d59221a3314
Marc <mf@thefuture.at>
2021-04-26 10:30:13 +0200

    save

--- 20210426_Zahlentraeume/src/at/cb/zahlen/Zahlentraeume.java
+++ 20210426_Zahlentraeume/src/at/cb/zahlen/Zahlentraeume.java
@@ -2,7 +2,7 @@ package at.cb.zahlen;
 
 public class Zahlentraeume {
     public static void main(String[] args) {
-        int zahl = 5;
+        int zahl = 7;
         if(zahl >= 1 && zahl <= 5){
             System.out.println("Die Zahl liegt zwischen 1 und 5");
         } else if(zahl >= 6 && zahl <= 10){
@@ -16,5 +16,21 @@ public class Zahlentraeume {
             System.out.println("Die Zahl ist zu groß oder zu klein");
         }
 
+        if(zahl >= 1 && zahl <= 5){
+            System.out.println("Die Zahl liegt zwischen 1 und 5");
+        }
+        if(zahl >= 6 && zahl <= 10){
+            System.out.println("Die Zahl liegt zwischen 6 und 10");
+            if(zahl == 10){
+                System.out.println("Jackpot");
+            }
+        }
+        if(zahl == 0){
+            System.out.println("Die Zahl darf nicht 0 sein");
+        }
+        if(zahl < 0 || zahl > 10){
+            System.out.println("Die Zahl ist zu groß oder zu klein");
+
+        }
     }
 }
