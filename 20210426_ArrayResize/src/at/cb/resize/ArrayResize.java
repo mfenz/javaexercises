16d253730bc22dec565bbc46c7e6edf5c141d617
Marc <mf@thefuture.at>
2021-04-26 21:21:37 +0200

    array resize

---
+++ 20210426_ArrayResize/src/at/cb/resize/ArrayResize.java
@@ -0,0 +1,12 @@
+package at.cb.resize;
+
+import java.util.Scanner;
+
+public class ArrayResize {
+    public static void main(String[] args) {
+        String[] produkte = new String[3];
+        int gespeicherteProdukte = 0;
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
