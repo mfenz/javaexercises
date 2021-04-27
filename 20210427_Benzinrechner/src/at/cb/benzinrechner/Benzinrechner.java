3f1425227cdb8dc69a87c45f6d6f2650e9875df7
Marc <mf@thefuture.at>
2021-04-27 10:48:59 +0200

    Benzinrechner

--- 20210427_Benzinrechner/src/at/cb/benzinrechner/Benzinrechner.java
+++ 20210427_Benzinrechner/src/at/cb/benzinrechner/Benzinrechner.java
@@ -1,10 +1,13 @@
 package at.cb.benzinrechner;
 
+import at.cb.benzinrechner.bl.BenzinrechnerBl;
+
 import java.util.Scanner;
 
 public class Benzinrechner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        BenzinrechnerBl bl = new BenzinrechnerBl(scanner);
         while(true){
             System.out.println("add: Tankvorgang eingeben");
             System.out.println("list: Tankvorgänge ausgeben");
@@ -14,10 +17,13 @@ public class Benzinrechner {
             String input = scanner.nextLine();
             switch (input){
                 case "add":
+                    bl.addTankvorgang();
                     break;
                 case "list":
+                    bl.listTankvorgaenge();
                     break;
                 case "stats":
+                    bl.printStatistik();
                     break;
                 case "exit":
                     System.exit(0);
