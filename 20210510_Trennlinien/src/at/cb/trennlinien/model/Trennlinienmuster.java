2a67cd26de2fc624fafb2b2dcbe5758ced6bad13
Marc <mf@thefuture.at>
2021-05-10 14:43:11 +0200

    Trennlinienmuster

--- 20210510_Trennlinien/src/at/cb/trennlinien/model/Trennlinienmuster.java
+++ 20210510_Trennlinien/src/at/cb/trennlinien/model/Trennlinienmuster.java
@@ -21,7 +21,27 @@ public class Trennlinienmuster {
      * Ausgabe des Trennlinienmusters
      */
     public void print(){
+        // For-Schleife über alle Zeilen
+        for(int zeile = 0; zeile < zeilen; zeile++){
+            // Die aktuelel Zeile ist in Variable zeile
 
-
+            // for(INIT; SCHLEIFENBEDINGUNG; VERÄNDERUNG){ SCHLEIFENKÖRPER }
+            // spalten = 10
+            for(int spalte = 0; spalte < spalten; spalte++){
+                // Schleifenkörper
+                if(zeile % 2 == 0 && spalte % 2 == 0){
+                    System.out.print("-");
+                } else if(zeile % 2 == 0 && spalte % 2 == 1){
+                    System.out.print(" ");
+                } else if(zeile % 2 == 1 && spalte % 2 == 0){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("-");
+                }
+            }
+            // Code nach der Schleife
+            // Nach der Ausgabe aller Spalten --> Zeilenumbruch machen
+            System.out.println();
+        }
     }
 }
