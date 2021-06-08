f30c6f7d6d55e6a1bb653968ef6a5b9293980c08
Marc <mf@thefuture.at>
2021-06-08 12:26:35 +0200

    user db

--- 20210608userverwaltung/src/main/java/at/cb/userverwaltung/bl/UserVerwaltungBl.java
+++ 20210608userverwaltung/src/main/java/at/cb/userverwaltung/bl/UserVerwaltungBl.java
@@ -5,7 +5,10 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserVerwaltungBl {
@@ -59,6 +62,20 @@ public class UserVerwaltungBl {
                         "(name) " +
                         "VALUES " +
                         "(?)");
+
+        /**
+         * INSERT INTO user
+         * (name)
+         * VALUES
+         * (Marc)
+         *
+         *
+         * INSERT INTO user
+         * (name)
+         * VALUES
+         * ('Marc\'); DELETE FROM user; (')
+         */
+
         // ? durch Wert ersetzen
         ps.setString(1, name);
 
@@ -69,6 +86,38 @@ public class UserVerwaltungBl {
     }
 
     public void listUsers(){
+        try {
+            List<User> users = findUsers();
+            for (User u : users){
+                System.out.printf("%d %s %n", u.getId(), u.getName());
+            }
+        } catch (SQLException e){
+            System.out.println("Fehler beim Laden der User: " + e.getMessage());
+        }
+    }
 
+    /**
+     * Gibt alle User in der Datenbank zurück
+     * @return
+     */
+    public List<User> findUsers() throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(
+                "SELECT id, name " +
+                        "FROM user ");
+        ResultSet rs = ps.executeQuery();
+
+        List<User> result = new ArrayList<>();
+
+        while (rs.next()){
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+
+            // Objekt der Klasse User erstellen
+            User u = new User(id, name);
+            result.add(u);
+        }
+
+        ps.close();
+        return result;
     }
 }
